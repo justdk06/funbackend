64f63c2c73e215cb308675006c0d93e954c59d3d
Dharmendra <23f2000072@ds.study.iitm.ac.in>
2025-06-12 17:17:46 +0530

    commit index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
 
-const port = 3000
+const port = 4000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.get('/twitter', (req, res)=>{
+    res.send('dharmendradotcom')
+})
+
+app.get('/login', (req, res)=>{
+    res.send('<h1>Please login at chai aur code')
+})
+
+app.get('/youtube',(req,res) =>{
+    res.send('<h2>Chai aur code</h2>')
+})
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
